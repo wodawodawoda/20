7af0de8cd32d111da933332724803c6ab76bfac8
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-04-28 14:12:30 +0200

    Add basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import {Router, Route, IndexRoute, hashHistory, Link} from 'react-router'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>React App</p>
-      </div>
+      <Router history={hashHistory}>
+        <Route path='/' component={Navigation}>
+          <IndexRoute component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="*" component={PageNotFound}/>
+        </Route>
+      </Router>
     );
   }
 }
+
+const Home = () => <h1>I'm Home component</h1>
+const Contact = () => <h1>I'm Contact component</h1>
+const Navigation = props => (
+  <div>
+    <Link to="/">Home</Link>
+    <Link to="/contact">Contact</Link>
+    {props.children}
+  </div>
+)
+const PageNotFound = () => <h1>I'm PageNotFound component</h1>
 
 export default App;
