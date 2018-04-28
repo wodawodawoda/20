876546030469e891205fb0c0c5d5bb31de47de92
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-04-28 17:19:00 +0200

    Create nav route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import App from './containers/App';
-import store from './store/index'
+import {Router, Route, hashHistory} from 'react-router'
+import store from './store/index';
 import DevTools from './store/devTools'
 import registerServiceWorker from './registerServiceWorker';
+import routes from './routes';
 
 ReactDOM.render(
   <Provider store={store}>
     <div>
-      <App />
+      <Router history={hashHistory} routes={routes}/>
       <DevTools />
     </div>
   </Provider>,
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Route} from 'react-router';
+import Navigation from './presentational/Navigation';
+
+export default (
+  <Route path="/" component={Navigation}></Route>
+)
