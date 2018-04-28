e265851bea236cfcb8c555c2c39c074104da6b72
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-04-28 20:16:13 +0200

    Add routes to new components, Fix bootstrap navigation js dependency

--- src/presentational/Navigation.js
+++ src/presentational/Navigation.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import {Link} from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-grid.css';
+import '../styles/navigation.css'
+
 
 const Navigation = props => (
   <div>
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <Link className="navbar-brand">Państwa.js</Link>
-        </div>
-        <div className="collapse navbar-collapse">
-          <ul className="nav navbar-nav">
-            <li><Link to="/countries">Countries</Link></li>
-            <li><Link to="/continents">Continents</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+    <nav className="navbar navbar-expand-lg navbar-light">
+        <Link className="navbar-brand">Logo</Link>
+        <input className="navbar-toggler navbar-toggler-icon"
+                type="checkbox"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation" />
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item"><Link to="/countries" className="nav-link">Countries</Link></li>
+            <li className="nav-item"><Link to="/continents" className="nav-link">Continents</Link></li>
+            <li className="nav-item"><Link to="/contact" className="nav-link">Contact</Link></li>
           </ul>
         </div>
-      </div>
     </nav>
     <div className="container-fluid">
       {props.children}
@@ -26,3 +30,6 @@ const Navigation = props => (
 )
 
 export default Navigation
+
+
+// data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(0, 0, 0, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {Route} from 'react-router';
+import {Route, IndexRoute} from 'react-router';
 import Navigation from './presentational/Navigation';
+import Home from './presentational/Home';
+import Contact from './presentational/Contact';
+import NotFound from './presentational/NotFound';
 
 export default (
   <Route path="/" component={Navigation}>
-    
+    <IndexRoute component={Home}/>
+    <Route path="contact" component={Contact}/>
+    <Route path="*" component={NotFound}/>
   </Route>
 )
