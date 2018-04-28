0c35d87845c6b5b7eb062e10a2c44bf01a49a14a
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-04-28 14:23:42 +0200

    Add Hello, 404 page component

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ class App extends Component {
         <Route path='/' component={Navigation}>
           <IndexRoute component={Home} />
           <Route path="/contact" component={Contact} />
+          <Route path="/hello/:name" component={Hello} />
           <Route path="*" component={PageNotFound}/>
         </Route>
       </Router>
@@ -19,11 +20,14 @@ const Home = () => <h1>I'm Home component</h1>
 const Contact = () => <h1>I'm Contact component</h1>
 const Navigation = props => (
   <div>
-    <Link to="/">Home</Link>
-    <Link to="/contact">Contact</Link>
+    <Link to="/" activeStyle={{color: '#992323'}} activeClassName={'active'} onlyActiveOnIndex>Home</Link>
+    <Link to="/contact" activeStyle={{color: '#992323'}} activeClassName={'active'} onlyActiveOnIndex>Contact</Link>
+    <Link to="/hello/user" activeStyle={{color: '#992323'}} activeClassName={'active'} onlyActiveOnIndex>Hello</Link>
     {props.children}
   </div>
 )
+const Hello = props => <h1>I'm Hello component. Welcome {props.params.name}!</h1>
+
 const PageNotFound = () => <h1>I'm PageNotFound component</h1>
 
 export default App;
